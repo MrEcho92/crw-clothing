8378f20d408b2962384871947598d454a4f92efb
Chigozie Onyenanu <Emmanuel@Chigozies-Air.lan>
2021-02-15 22:31:11 +0000

    implemented firebase utils, included ability to authenticate users to database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-
+// Config detail from google 
 const config = {
   apiKey: "AIzaSyBTFq8Ov2NBIrqEETA7xRafYjT9eUtMnVk",
   authDomain: "crwn-db-59167.firebaseapp.com",
@@ -12,6 +12,36 @@ const config = {
   appId: "1:1028538549789:web:89503c6c937c61470528f4",
   measurementId: "G-SBR9TR9375",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+  if (!userAuth){
+    return;
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try{ 
+      await userRef.set({  
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch(error){
+       console.log('error creating user', error.message); 
+    }
+  }
+  return userRef;
+
+}
+
+
+
+// from firebase documentation
 
 firebase.initializeApp(config);
 
