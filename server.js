aecb2a06e1f4070e8c28f8f93cb62f7da67b4a33
Chigozie Onyenanu <Emmanuel@Chigozies-Air.lan>
2021-03-26 18:06:38 +0000

    updated server.js with enforce

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+var enforce = require("express-sslify");
 // const compression = require('compression');
 
 
